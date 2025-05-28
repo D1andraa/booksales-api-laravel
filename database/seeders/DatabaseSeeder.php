a22f2c00cac721e8b31726a52027cd05d8c444c7
MohamadDiandraFerdiansyah <202210715118@mhs.ubharajaya.ac.id>
2025-05-28 10:23:43 +0700

    Tugas 7 Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             BookSeeder::class,
             AuthorSeeder::class,
             UserSeeder::class,
+            TransactionSeeder::class
         ]);
     }
 }
